194dba83d7eb0b1a1b1c8f5d80460543edd0fa9c
Amey <72331282+vazurkar@users.noreply.github.com>
2026-09-22 17:46:48 +0530

    added circuit breaker and redis cache

--- EmployeeService/src/main/java/com/employee/EmployeeService/Service/EmployeeService.java
+++ EmployeeService/src/main/java/com/employee/EmployeeService/Service/EmployeeService.java
@@ -10,6 +10,7 @@ import com.employee.EmployeeService.Repository.EmployeeRepo;
 import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,9 +22,11 @@ import java.util.Objects;
 public class EmployeeService {
 
     private final EmployeeRepo employeeRepo;
-
     private final ModelMapper modelMapper;
     private final AddressClient addressClient;
+
+    @Autowired
+    private RedisService redisService;
 
     public EmployeeService(EmployeeRepo employeeRepo, ModelMapper modelMapper, AddressClient addressClient) {
         this.employeeRepo = employeeRepo;
@@ -32,18 +35,16 @@ public class EmployeeService {
     }
 
     public EmployeeDTO addEmployee(EmployeeDTO employeeDTO) {
-        if(employeeDTO.getId() != null){
+        if (employeeDTO.getId() != null) {
             throw new RuntimeException();
         }
         Employee emp = modelMapper.map(employeeDTO, Employee.class);
         Employee newEmp = employeeRepo.save(emp);
         return modelMapper.map(newEmp, EmployeeDTO.class);
-
-
     }
 
-    public EmployeeDTO updateEmployee(Long id,EmployeeDTO employeeDTO) {
-        if(id == null || employeeDTO.getId() == null || !Objects.equals(employeeDTO.getId(), id)){
+    public EmployeeDTO updateEmployee(Long id, EmployeeDTO employeeDTO) {
+        if (id == null || employeeDTO.getId() == null || !Objects.equals(employeeDTO.getId(), id)) {
             throw new BadRequestException("Please provide correct employee id");
         }
         Employee emp = modelMapper.map(employeeDTO, Employee.class);
@@ -51,42 +52,49 @@ public class EmployeeService {
         return modelMapper.map(newEmp, EmployeeDTO.class);
     }
 
-    public void deleteEmployee(Long id){
+    public void deleteEmployee(Long id) {
         Employee emp = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found" + id));
         employeeRepo.delete(emp);
     }
 
-    public EmployeeDTO getEmployee(Long id){
-        Employee emp = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found" + id));
+    public EmployeeDTO getEmployee(Long id) {
 
-        List<AddressDTO>addresses = new ArrayList<>();
-        try{
-            addresses =  addressClient.getAddressByEmpId(id);
-        }   catch (
-    FeignException.NotFound e) {
-        log.error("Address not found with id " +id);
+        EmployeeDTO employeeDTO = redisService.getEmployee(id, EmployeeDTO.class);
+        if (employeeDTO != null) {
+            log.info("REsult returend from cache");
+            return employeeDTO;
+        } else {
+            Employee emp = employeeRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found" + id));
+
+            List<AddressDTO> addresses = new ArrayList<>();
+            try {
+                addresses = addressClient.getAddressByEmpId(id);
+            } catch (FeignException.NotFound e) {
+                log.error("Address not found with id " + id);
+            }
+
+            EmployeeDTO dto = modelMapper.map(emp, EmployeeDTO.class);
+            dto.setAddresses(addresses);
+            redisService.setEmployee(id, dto, 300L);
+            log.info("REsult returend from database");
+            return dto;
+        }
     }
 
-        EmployeeDTO dto= modelMapper.map(emp, EmployeeDTO.class);
-        dto.setAddresses(addresses);
-        return dto;
-    }
-
-    public List<EmployeeDTO> getAllEmployees(){
+    public List<EmployeeDTO> getAllEmployees() {
 
         List<Employee> employees = employeeRepo.findAll();
-        if(employees.isEmpty()){
+        if (employees.isEmpty()) {
             throw new ResourceNotFoundException("Employee not found");
         }
-        List<EmployeeDTO> empployeeDtoList =  employees.stream().map(emp -> modelMapper.map(emp,EmployeeDTO.class)).toList();
+        List<EmployeeDTO> empployeeDtoList = employees.stream().map(emp -> modelMapper.map(emp, EmployeeDTO.class)).toList();
         List<EmployeeDTO> response = new ArrayList<>();
-        for(EmployeeDTO employeeDTO : empployeeDtoList){
-            List<AddressDTO>addresses = new ArrayList<>();
-            try{
-                addresses =  addressClient.getAddressByEmpId(employeeDTO.getId());
-            }   catch (
-                    FeignException.NotFound e) {
-                log.error("Address not found with id " +employeeDTO.getId());
+        for (EmployeeDTO employeeDTO : empployeeDtoList) {
+            List<AddressDTO> addresses = new ArrayList<>();
+            try {
+                addresses = addressClient.getAddressByEmpId(employeeDTO.getId());
+            } catch (FeignException.NotFound e) {
+                log.error("Address not found with id " + employeeDTO.getId());
             }
             employeeDTO.setAddresses(addresses);
         }
@@ -94,12 +102,8 @@ public class EmployeeService {
         return response;
     }
 
-
     public EmployeeDTO getEmployeeByEmpCodeAndCompanyName(String empCode, String companyName) {
-
-        Employee emp = employeeRepo.findByEmpCodeAndCompanyName(empCode,companyName).orElseThrow(()-> new ResourceNotFoundException("Employee not found"));
+        Employee emp = employeeRepo.findByEmpCodeAndCompanyName(empCode, companyName).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
         return modelMapper.map(emp, EmployeeDTO.class);
-
-
     }
 }
